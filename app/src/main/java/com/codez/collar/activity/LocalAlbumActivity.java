249e1dd7fc85ccfe5c03398e7bdcfa35b0c4ae7f
codez <786359226@qq.com>
2017-12-02 01:48:35 +0800

    1、本地相册选择界面：添加各图片的选择和取消状态切换，并对当前选择图片的数目绑定监听器，监听当前选择的图片数，从而实现当前"完成（n/9）"按钮文字内容的改变以及按钮状态的切换 2、本地相册选择界面：修改列表图片adapter的逻辑，使第一个位置显示拍照指示图，并指示点击跳转到拍照界面，后序需补充跳转拍照界面功能

--- app/src/main/java/com/codez/collar/activity/LocalAlbumActivity.java
+++ app/src/main/java/com/codez/collar/activity/LocalAlbumActivity.java
@@ -10,27 +10,37 @@ import com.codez.collar.R;
 import com.codez.collar.adapter.LocalAlbumAdapter;
 import com.codez.collar.base.BaseActivity;
 import com.codez.collar.bean.AlbumFolder;
-import com.codez.collar.databinding.ActivityLocaAlbumBinding;
+import com.codez.collar.databinding.ActivityLocalAlbumBinding;
 import com.codez.collar.tools.ImageScan;
 import com.codez.collar.utils.DensityUtil;
+import com.codez.collar.utils.T;
 
 import java.io.File;
 import java.util.ArrayList;
 
-public class LocalAlbumActivity extends BaseActivity<ActivityLocaAlbumBinding> implements View.OnClickListener{
+public class LocalAlbumActivity extends BaseActivity<ActivityLocalAlbumBinding> implements View.OnClickListener{
 
     private ArrayList<AlbumFolder> mList = new ArrayList<>();
     private LocalAlbumAdapter mAdapter;
     @Override
     public int setContent() {
-        return R.layout.activity_loca_album;
+        return R.layout.activity_local_album;
     }
 
     @Override
     public void initView() {
         setToolbarTitle(mBinding.toolbar, "所有");
 
+        mBinding.setCount(0);
+
         mAdapter = new LocalAlbumAdapter(this);
+        mAdapter.addSelectedCountChangedListener(new LocalAlbumAdapter.OnSelectedCountChangedListener() {
+            @Override
+            public void onChanged(int count) {
+                mBinding.tvCommit.setSelected(count>0);
+                mBinding.setCount(count);
+            }
+        });
         mBinding.recyclerView.setAdapter(mAdapter);
         mBinding.recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
         mBinding.swipeRefreshLayout.setRefreshing(true);
@@ -64,6 +74,8 @@ public class LocalAlbumActivity extends BaseActivity<ActivityLocaAlbumBinding> i
                 mAdapter.notifyDataSetChanged();
             }
         };
+
+        mBinding.tvCommit.setOnClickListener(this);
     }
 
 
@@ -71,7 +83,10 @@ public class LocalAlbumActivity extends BaseActivity<ActivityLocaAlbumBinding> i
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-
+            case R.id.tv_commit:
+                //TODO
+                T.s(this, "完成");
+                break;
         }
     }
 }
--- app/src/main/java/com/codez/collar/adapter/LocalAlbumAdapter.java
+++ app/src/main/java/com/codez/collar/adapter/LocalAlbumAdapter.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bumptech.glide.Glide;
 import com.codez.collar.R;
 import com.codez.collar.databinding.ItemLocalAlbumBinding;
 import com.codez.collar.utils.ScreenUtil;
@@ -25,10 +26,13 @@ public class LocalAlbumAdapter extends RecyclerView.Adapter<LocalAlbumAdapter.Bi
 
     private Context mContext;
     private List<String> list;
+    private int mSelectedCount;
+    private static final int SELECTED_COUNT = 9;
 
     public LocalAlbumAdapter(Context mContext) {
         this.mContext = mContext;
         this.list = new ArrayList<>();
+        this.mSelectedCount = 0;
     }
 
     @Override
@@ -45,12 +49,12 @@ public class LocalAlbumAdapter extends RecyclerView.Adapter<LocalAlbumAdapter.Bi
 
     @Override
     public void onBindViewHolder(BindingViewHolder holder, int position) {
-        holder.bindItem(list.get(position), position);
+        holder.bindItem(position);
     }
 
     @Override
     public int getItemCount() {
-        return list.size();
+        return list.size()+1;
     }
 
 
@@ -59,21 +63,48 @@ public class LocalAlbumAdapter extends RecyclerView.Adapter<LocalAlbumAdapter.Bi
         private ItemLocalAlbumBinding mBinding;
 
         public BindingViewHolder(ItemLocalAlbumBinding binding) {
-            super(binding.llRoot);
+            super(binding.getRoot());
             this.mBinding = binding;
         }
-        private void bindItem(final String uri, final int postition){
-            mBinding.ivPic.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    T.s(mContext, "click:"+uri);
-                }
-            });
-            mBinding.setUri(uri);
-            GridLayoutManager.LayoutParams params = (GridLayoutManager.LayoutParams) mBinding.llRoot.getLayoutParams();
+        private void bindItem(final int postition){
+            if (postition == 0) {
+                //此处为拍照项，可点击跳转拍照界面
+                mBinding.ivTakePhoto.setVisibility(View.VISIBLE);
+                mBinding.rlPic.setVisibility(View.GONE);
+                Glide.with(mContext)
+                        .load(R.drawable.pic_album_take_photo)
+                        .into(mBinding.ivTakePhoto);
+                mBinding.ivTakePhoto.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //Todo:跳转拍照界面
+                        T.s(mContext, "TODO跳转拍照");
+                    }
+                });
+            }else{
+                mBinding.ivPic.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (mBinding.ivSelect.isSelected()) {
+                            mBinding.ivSelect.setSelected(false);
+                            mSelectedCount--;
+                            onSelectedCountChangedListener.onChanged(mSelectedCount);
+                        } else if (mSelectedCount == SELECTED_COUNT) {
+                            T.s(mContext, "最多只能选择9张图片");
+                        } else {
+                            mBinding.ivSelect.setSelected(true);
+                            mSelectedCount++;
+                            onSelectedCountChangedListener.onChanged(mSelectedCount);
+                        }
+                    }
+                });
+                mBinding.setUri(list.get(postition-1));
+            }
+
+            GridLayoutManager.LayoutParams params = (GridLayoutManager.LayoutParams) mBinding.getRoot().getLayoutParams();
             params.width = (int) (ScreenUtil.getScreenWidth(mContext)*0.31);
             params.height = (int) (ScreenUtil.getScreenWidth(mContext)*0.31);
-            mBinding.llRoot.setLayoutParams(params);
+            mBinding.getRoot().setLayoutParams(params);
 
             mBinding.executePendingBindings();
         }
@@ -89,5 +120,13 @@ public class LocalAlbumAdapter extends RecyclerView.Adapter<LocalAlbumAdapter.Bi
 
     public List<String> getList() {
         return list;
+    }
+
+    private OnSelectedCountChangedListener onSelectedCountChangedListener;
+    public void addSelectedCountChangedListener(OnSelectedCountChangedListener listener) {
+        this.onSelectedCountChangedListener = listener;
+    }
+    public interface OnSelectedCountChangedListener {
+        void onChanged(int count);
     }
 }
